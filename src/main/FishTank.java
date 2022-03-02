18c768e568ec88f52710f8cc801a4aa08a272926
István Orosz <oi5672@gmail.com>
2022-03-02 13:42:38 +0000

    add removeFish method to FishTank

--- src/main/FishTank.java
+++ src/main/FishTank.java
@@ -1,18 +1,25 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class FishTank {
    private int volumeInLiter;
+   private double bigFishLimitInGram;
+
    private List<Fish> fishes = new ArrayList<>();
    
-   public FishTank(int volumeInLiter){
+   public FishTank(int volumeInLiter, double bigFishLimitInGram){
        this.volumeInLiter = volumeInLiter;
+       this.bigFishLimitInGram = bigFishLimitInGram;
    }
 
    public int getVolumeInLiter() {
        return this.volumeInLiter;
+   }
+   public double getBigFishLimitInGram(){
+       return this.bigFishLimitInGram;
    }
 
    public void addFish(Fish fish){
@@ -22,4 +29,12 @@ public class FishTank {
    public int getNumberOfFish(){
        return this.fishes.size();
    }
+
+   public void removeFish(){
+        for (Iterator<Fish> iterator = this.fishes.iterator(); iterator.hasNext(); ) {
+            Fish fish = iterator.next();
+            if (fish.getWeightInGram() >= this.bigFishLimitInGram)
+                iterator.remove();
+        }
+    }
 }
--- src/test/AppTest.java
+++ src/test/AppTest.java
@@ -7,10 +7,14 @@ import main.Fish;
 import main.FishTank;
 
 public class AppTest {
+    int TANK_CAPACITY = 240;
+    double BIG_FISH = 11;
+
     @Test
     public void createFishTankWith240Lcapacity(){
-        FishTank tank = new FishTank(240);
-        assertEquals(240, tank.getVolumeInLiter());
+        FishTank tank = new FishTank(TANK_CAPACITY, BIG_FISH);
+        assertEquals(TANK_CAPACITY, tank.getVolumeInLiter());
+        assertEquals(BIG_FISH, tank.getBigFishLimitInGram(), 0);
     }
 
     @Test
@@ -22,11 +26,25 @@ public class AppTest {
     }
 
     @Test
-    public void addOneFishToTank(){
-        FishTank tank = new FishTank(240);
+    public void addFishToTank(){
+        FishTank tank = new FishTank(TANK_CAPACITY, BIG_FISH);
         assertEquals(0, tank.getNumberOfFish());
-        Fish fish = new Carp("Pontyi", 1, "silver");
-        tank.addFish(fish);
+        Fish fish1 = new Carp("Pontyi", 1, "silver");
+        tank.addFish(fish1);
         assertEquals(1, tank.getNumberOfFish());
+        Fish fish2 = new Carp("Potty", 2, "orange");
+        tank.addFish(fish2);
+        assertEquals(2, tank.getNumberOfFish());
     }
+
+    @Test
+    public void removeBigFishFromTank(){
+        FishTank tank = new FishTank(TANK_CAPACITY, BIG_FISH);
+        tank.addFish(new Carp("big1", BIG_FISH, "c1"));
+        tank.addFish(new Carp("small", 2, "c2"));
+        tank.addFish(new Carp("big2", BIG_FISH+1, "c3"));
+        assertEquals(3, tank.getNumberOfFish());
+        tank.removeFish();
+        assertEquals(1, tank.getNumberOfFish());
+    }    
 }
